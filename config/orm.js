0d7ddd931b220f48c59f62cd800f5c84b93544d6
jjmonte20 <jjmonte20@gmail.com>
2018-12-06 18:23:43 -0600

    passing through cb on the orm.js page to have it get to the model page

--- config/orm.js
+++ config/orm.js
@@ -4,32 +4,32 @@ const connection = require("./connection.js");
 // need to make a variable called orm
 var orm = {
     // want a catch all to select all
-    selectAll: function(tableName){
+    selectAll: function(tableName, cb){
         var queryString = "SELECT * FROM ??";
         connection.query(queryString, [tableName], function (err, data) {
             if (err) throw err;
-            console.log(data);
+            cb(data);
         });
     },
 
     // want a catch all to add a burger
-    insertInto: function(tableName, abName, rowName){
+    insertInto: function(tableName, abName, rowName, cb){
         // making a string that should allow users to add a burger, will automatically not be eaten though
         var queryString = "INSERT INTO ?? (??) VALUES (?, false)";
         connection.query(queryString, [tableName, abName, rowName], function (err, data) {
             if (err) throw err;
-            console.log(data);
+            cb(data);
         });
     },
 
     // want a catch all for updating a burger from not eaten to eaten
-    eatBurger: function(tableName, col, boole, abId, brgrId) {
+    eatBurger: function(tableName, col, boole, abId, brgrId, cb) {
         // this is meant to update a burger from burgers table to eaten = true where the id is the identifier,
         // would use name, but do not want a situation where 2 burgers of the same name get eaten at the same time
         var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
         connection.query(queryString, [tableName, col, boole, abId, brgrId], function(err, data) {
             if (err) throw err;
-            console.log(data);
+            cb(data);
         });
     }
 }
--- models/burger.js
+++ models/burger.js
@@ -3,9 +3,20 @@ require("./../config/orm.js");
 
 // in the model I want to have statements for when the burger needs to show, one is created, and one updates
 var burger = {
-    all: function(cb) {
+    // this function shows all of the burgers
+    selectAll: function(cb) {
         // using orm to have a templete model that works in a templete
-    }
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    // this function creates a new burger
+    insertInto: function(abName, rowName, cb) {
+        orm.insertInto("burgers", abName, rowName, function(res) {
+            cb(res);
+        });
+    },
+    // this function updates the burgers to be eaten
 };
 
 // export the function to the controller
