b78a34657d9330a40a9f36af6fb6343d32f4d008
jjmonte20 <jjmonte20@gmail.com>
2018-12-07 16:40:23 -0600

    added JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
 // need to require node to sql
 const mysql = require("mysql");
+// making connection dynamic so it can be used locally or online
+var connection;
 
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 // want to export the function so it can be used elsewhere
 
 // now I want to connect mysql to by database
-const connection = mysql.createConnection({
-    // host should be local
-    host: "localhost",
-    // port number
-    port: 3306,
-    // user name
-    user: "root",
-    // password, in case it changes
-    password: "EpicGis20",
-    // what database am I connecting to
-    database: "burgerDB"
-});
+    connection = mysql.createConnection({
+        // host should be local
+        host: "localhost",
+        // port number
+        port: 3306,
+        // user name
+        user: "root",
+        // password, for use on a local server
+        password: "",
+        // what database am I connecting to
+        database: "burgerDB"
+    });
+};
 
 // then connect to the database
 connection.connect(function(err){
